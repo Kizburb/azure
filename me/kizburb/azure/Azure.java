3280b870e9209487f9848e58cf2d92cf13851f34
Unknown <kizburb@gmail.com>
2018-12-14 01:46:23 +0100

    Setting up.

--- me/kizburb/azure/Azure.java
+++ me/kizburb/azure/Azure.java
@@ -1,5 +1,15 @@
-package me.kizburb.azure;
+package azure.me.kizburb.azure;
+
+import org.lwjgl.opengl.Display;
 
 public class Azure {
 
+	public String name = "Azure", version = "1", creator = "Kizburb";
+
+	public static Azure INSTANCE = new Azure();
+
+	public void startClient() {
+		System.out.println("[" + name + "] Starting client Build " + version + ", by " + creator);
+		Display.setTitle(name + " | Build " + version);
+	}
 }
